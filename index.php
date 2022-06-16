ac4440fbab36a9f51a79d0d15a0696ddfea42c35
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-06-16 22:29:38 +0200

    Updating styling img

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
+<?php
+if (isset($_POST['poke'])) {
+    $searchPoke = $_POST['poke'];
+    if (empty($searchPoke)) {header("Refresh:0");}
+    $api_url = 'https://pokeapi.co/api/v2/pokemon/' . strtolower($searchPoke);
 
-<!doctype html>
+//READ THE JSON FILE
+    $json_data = file_get_contents($api_url);
+    if (!$json_data){ echo "<h1>No encontre tu pokémon</h1>";return ;}
+
+//DECODE JSON DATA INTO PHP ARRAY
+    $response_data = json_decode($json_data, true);
+    $pokeImage = $response_data['sprites']['front_shiny'];
+
+    if (!$pokeImage) return;
+    // using pre to make api array more visible
+    //echo"<pre>";
+
+// this is how to filter in api with array
+    $nameStr= "Name:";
+    $name = $response_data['forms']['0']['name'];
+    $movesStr= "Moves:";
+    $moves = $response_data['moves']['0']['move']['name'];
+    $abilitiesStr= "Abilities:";
+    $abilities = $response_data['abilities']['0']['ability']['name'];
+    //$sprites = $response_data['sprites']['other']['home']['front_default'];
+    $idStr="Id:";
+    $id = $response_data['id'];
+    $src = "src";
+
+//this is an example to filter in api with object
+//print_r($response_data->moves);
+}
+?>
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -12,41 +45,6 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    if (isset($_POST['poke'])) {
-        $searchPoke = $_POST['poke'];
-        if (empty($searchPoke)) {header("Refresh:0");}
-        $api_url = 'https://pokeapi.co/api/v2/pokemon/' . strtolower($searchPoke);
-
-//READ THE JSON FILE
-        $json_data = file_get_contents($api_url);
-        if (!$json_data){ echo "<h1>No encontre tu pokémon</h1>";return ;}
-
-//DECODE JSON DATA INTO PHP ARRAY
-        $response_data = json_decode($json_data, true);
-        $pokeImage = $response_data['sprites']['front_shiny'];
-        if (!$pokeImage) return;
-        // using pre to make api array more visible
-        //echo"<pre>";
-
-// this is how to filter in api with array
-        $nameStr= "Name:";
-        $name = $response_data['forms']['0']['name'];
-        $movesStr= "Moves:";
-        $moves = $response_data['moves']['0']['move']['name'];
-        $abilitiesStr= "Abilities:";
-        $abilities = $response_data['abilities']['0']['ability']['name'];
-        //$sprites = $response_data['sprites']['other']['home']['front_default'];
-        $idStr="Id:";
-        $id = $response_data['id'];
-
-//this is an example to filter in api with object
-//print_r($response_data->moves);
-
-
-}
-?>
-
 <div class="container-main" >
         <div class="header">
             <div><h1>Pokedex</h1></div>
@@ -129,7 +127,7 @@
                 </div>
                 <div class="images">
                     <div class="sprites">
-                        <img src ="<?php
+                        <img <?php if(isset($_POST['poke'])){echo $src;}?> ="<?php
                         if (isset($_POST['poke'])) {
                             echo $pokeImage;
                         }?>">
